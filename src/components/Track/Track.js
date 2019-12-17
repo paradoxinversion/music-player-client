062963beeaee3cf1e40c7ef4fd946a80c304b648
Jedai Saboteur <dev@paradoxinversion.com>
2019-12-17 12:09:45 -0800

    Converted track to fn component with hook

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,39 +3,27 @@ import connect from "unstated-connect";
 import Icon from "@mdi/react";
 import { mdiPlay } from "@mdi/js";
 import AppContainer from "../../containers/AppContainer";
-class Track extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isHighlighted: false
-    };
-  }
 
-  render() {
-    const [AppContainer] = this.props.containers;
-    return (
-      <div
-        onMouseEnter={() => {
-          this.setState({ isHighlighted: true });
-        }}
-        onMouseLeave={event => {
-          this.setState({ isHighlighted: false });
-        }}
-        onClick={() => {
-          if (!AppContainer.state.isTrackLoading)
-            AppContainer.selectTrack(this.props.track.name);
-        }}
-        className="track">
-        {this.props.track.name.slice(0, this.props.track.name.length - 4)}
-
-        {this.state.isHighlighted && (
-          <React.Fragment>
-            <Icon path={mdiPlay} title="Play" size={1} color="white" />
-          </React.Fragment>
-        )}
-      </div>
-    );
-  }
-}
+const Track = props => {
+  const [AppContainer] = props.containers;
+  const [isHighlighted, setIsHighlighted] = useState(false);
+  return (
+    <div
+      onMouseEnter={() => setIsHighlighted(true)}
+      onMouseLeave={() => setIsHighlighted(false)}
+      onClick={() => {
+        if (!AppContainer.state.isTrackLoading)
+          AppContainer.selectTrack(props.track.name);
+      }}
+      className="track">
+      {props.track.name.slice(0, props.track.name.length - 4)}
+      {isHighlighted && (
+        <React.Fragment>
+          <Icon path={mdiPlay} title="Play" size={1} color="white" />
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
 
 export default connect([AppContainer])(Track);
